9d8f0283242024691eebefb444cf2b12eedb5982
wertikeo <wertikeo@yeah.net>
2024-12-13 12:35:56 +0800

    new22

--- test1.py
+++
@@ -1,9 +0,0 @@
-import requests
-url="https://www.imslr.com/member.php?mod=logging&action=login&loginsubmit=yes&handlekey=login&loginhash=LcUBc&inajax=1"
-params={
-    "username":"3179681062@qq.com",
-    "password":"Aoterman2000"
-}
-response=requests.post(url=url,data=params)
-print(response.content.decode())
-
